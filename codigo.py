16753f10ec3c1ebf03c1b13fec4c14c3d8bc17c2
pedromeixu <pedromeixu44@gmail.com>
2024-10-28 14:35:11 +0100

    opcion ordenador

--- codigo.py
+++ codigo.py
@@ -1,6 +1,13 @@
+import random
+
 def elegir_opcion() :
     print("elige una opcion\n", "1. Piedra\n", "2. Papel\n", "3. Tijera")
     opcion = int(input("escoge una opcion"))
     return opcion
 
+def opcion_ordenador () :
+    opcion_pc = random.randrange(1,3)
+    return opcion_pc
+
 print(elegir_opcion())
+print(opcion_ordenador())
