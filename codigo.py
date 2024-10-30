984ad1e23c262c69eb4a7f7568b84e059ca3136b
pedromeixu <pedromeixu44@gmail.com>
2024-10-30 10:33:17 +0100

    def ganador

--- codigo.py
+++ codigo.py
@@ -1,13 +1,30 @@
 import random
 
 def elegir_opcion() :
-    print("elige una opcion\n", "1. Piedra\n", "2. Papel\n", "3. Tijera")
-    opcion = int(input("escoge una opcion"))
+    print("elige una opcion\n", "Piedra\n", "Papel\n", "Tijera")
+    opcion = input("escoge una opcion")
     return opcion
 
 def opcion_ordenador () :
     opcion_pc = random.randrange(1,3)
     return opcion_pc
 
-print(elegir_opcion())
-print(opcion_ordenador())
+def ganador () :
+    if (elegir_opcion () == "Piedra" and opcion_ordenador () == 1) :
+        print("empate")
+    elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 2) :
+        print("perdiste")
+    elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 3) :
+        print("ganaste")
+    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 1) :
+        print("ganaste")
+    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 2) :
+        print("empate")
+    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 3) :
+        print("perdiste")
+    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 1) :
+        print("perdiste")
+    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 2) :
+        print("ganaste")
+    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 3) :
+        print("empate")
