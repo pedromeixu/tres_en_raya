fda393e5797500bf72c550a7df2f900c6cfddd0a
pedromeixu <pedromeixu44@gmail.com>
2024-11-04 15:18:20 +0100

    actu

--- codigo.py
+++ codigo.py
@@ -9,22 +9,26 @@ def opcion_ordenador () :
     opcion_pc = random.randrange(1,3)
     return opcion_pc
 
-def ganador () :
+def ganador (contador1 = 0, contador2 = 0) :
     if (elegir_opcion () == "Piedra" and opcion_ordenador () == 1) :
-        print("empate")
+        return "empate"
     elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 2) :
-        print("perdiste")
+        return "perdiste"
     elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 3) :
-        print("ganaste")
+        return "ganaste"
     elif (elegir_opcion () == "Papel" and opcion_ordenador () == 1) :
-        print("ganaste")
+        return "ganaste"
     elif (elegir_opcion () == "Papel" and opcion_ordenador () == 2) :
-        print("empate")
+        return "empate"
     elif (elegir_opcion () == "Papel" and opcion_ordenador () == 3) :
-        print("perdiste")
+        return "perdiste"
     elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 1) :
-        print("perdiste")
+        return "perdiste"
     elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 2) :
-        print("ganaste")
+        return "ganaste"
     elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 3) :
-        print("empate")
+        return "empate"
+
+while (contador1 < 3 or contador2 < 3) :
+    elegir_opcion ()
+    opcion_ordenador ()
