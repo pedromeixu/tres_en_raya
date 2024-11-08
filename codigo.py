4386cd0a373b73c6e105ec48acbe881ab13fcb17
pedromeixu <pedromeixu44@gmail.com>
2024-11-08 09:32:34 +0100

    mucho mejor

--- codigo.py
+++ codigo.py
@@ -9,40 +9,34 @@ def opcion_ordenador () :
     opcion_pc = random.randrange(1,4)
     return opcion_pc
 
-def ganador () :
-    if (elegir_opcion () == "Piedra" and opcion_ordenador () == 1) :
+def ganador (jugador, ordenador) :
+    if (jugador () == "Piedra" and ordenador () == 1) :
         return "empate"
-    elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 2) :
+    elif (jugador () == "Piedra" and ordenador() == 2) :
         return "perdiste"
-    elif (elegir_opcion () == "Piedra" and opcion_ordenador () == 3) :
+    elif (jugador () == "Piedra" and ordenador () == 3) :
         return "ganaste"
-    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 1) :
+    elif (jugador () == "Papel" and ordenador () == 1) :
         return "ganaste"
-    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 2) :
+    elif (jugador () == "Papel" and ordenador () == 2) :
         return "empate"
-    elif (elegir_opcion () == "Papel" and opcion_ordenador () == 3) :
+    elif (jugador () == "Papel" and ordenador () == 3) :
         return "perdiste"
-    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 1) :
+    elif (jugador () == "Tijera" and ordenador () == 1) :
         return "perdiste"
-    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 2) :
+    elif (jugador () == "Tijera" and ordenador () == 2) :
         return "ganaste"
-    elif (elegir_opcion () == "Tijera" and opcion_ordenador () == 3) :
+    elif (jugador () == "Tijera" and ordenador () == 3) :
         return "empate"
 
 contador1 = 0
 contador2 = 0
 
-opcion_ordenador ()
-
 while (contador1 < 3 or contador2 < 3) :
-    print (elegir_opcion ())
-    opcion_ordenador ()
-    print (ganador ())
-    if ganador () == "perdiste" :
-        print("perdiste la ronda")
-        contador2 = contador2 + 1
-    elif ganador () == "ganaste" :
-        print("ganaste la ronda")
-        contador1 == contador1 + 1
-    else :
-        print("empatasteis la ronda")
+    jugador = elegir_opcion ()
+    ordenador = opcion_ordenador ()
+
+    print ("tu eleccion ha sido", jugador, ", la del ordenador fue", ordenador)
+
+    resultado = ganador(jugador, ordenador)
+    print("resultado")
