71d2069af501cc56974ecaae99f6a6477dd681d7
pedromeixu <pedromeixu44@gmail.com>
2024-11-10 18:38:55 +0100

    me puse con el tema del ordenador fumeta

--- codigo.py
+++ codigo.py
@@ -9,6 +9,17 @@ def opcion_ordenador () :
     opcion_pc = random.randrange(1,4)
     return opcion_pc
 
+def ordenador_tramposo (jugador, opcion_pc) :
+    if jugador == "Piedra" :
+        opcion_pc == 2
+    elif jugador == "Papel" :
+        opcion_pc == 3
+    else :
+        opcion_pc == 1
+    print ("tramposo")
+    return opcion_pc
+ 
+
 def ganador (jugador, ordenador) :
     if (jugador == "Piedra" and ordenador == 1) :
         return "empate"
@@ -36,22 +47,29 @@ while (contador1 < 3 and contador2 < 3) :
     jugador = elegir_opcion ()
     ordenador = opcion_ordenador ()
 
+    if contador1 == 2 :
+        ordenador_tramposo (jugador, ordenador)
+    
     print ("tu eleccion ha sido", jugador, ", la del ordenador fue", ordenador)
 
     resultado = ganador(jugador, ordenador)
+
     print("resultado")
 
     if resultado == "perdiste" :
         print ("perdiste la ronda")
         contador2 = contador2 + 1
         print ("tu vas:", contador1, "\nel ordenador va:", contador2)
+        print()
     elif resultado == "ganaste" :
         print("ganaste la ronda") 
         contador1 = contador1 + 1
         print ("tu vas:", contador1, "\nel ordenador va:", contador2)
+        print ()
     else: 
         print("empataste la ronda")
         print ("tu vas:", contador1, "\nel ordenador va:", contador2)
+        print()
 
 if contador1 == 3:
     print("GANASTE!!!!!")
